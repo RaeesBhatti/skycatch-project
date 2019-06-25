e1ad3a49ea725ab939fadc4ba274047309c4338f
Raees Iqbal <raees.bhatti@gmail.com>
2019-06-25 21:24:25 +0500

    Fix some CSV formatting bugs

--- csvExporter/main.go
+++ csvExporter/main.go
@@ -16,8 +16,11 @@ import (
 	"os"
 )
 
+var keys []string
+
 func init() {
 	exif.RegisterParsers(mknote.All...)
+	keys = []string{}
 }
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
@@ -73,7 +76,6 @@ func Handler(ctx context.Context) error {
 
 func scan(ctx context.Context, db *dynamodb.Client, lek map[string]dynamodb.AttributeValue) ([][]string, []string, error) {
 	var data = [][]string{}
-	var keysMap = map[string]bool{}
 
 	var scanParams = &dynamodb.ScanInput{
 		TableName: aws.String(os.Getenv("DYNAMO_TABLE_IMAGE_DATA")),
@@ -98,22 +100,25 @@ func scan(ctx context.Context, db *dynamodb.Client, lek map[string]dynamodb.Attr
 
 	// Get the unique keys and data
 	for _, i := range sr.Items {
-		var item = []string{}
+		var item = make([]string, 0, len(i))
+		item = item[:cap(item)]
 		for k, v := range i {
-			keysMap[k] = true
+			index := findIndex(keys, k)
+			if index < 0 {
+				keys = append(keys, k)
+				index = len(keys) + 1
+			}
+			if cap(item) < index + 1 {
+				item = make([]string, 0, index + 1)
+				item = item[:cap(item)]
+			}
 			if v.NULL != nil && *v.NULL != false {
-				item = append(item, "")
+				item[index] = ""
 			} else {
-				item = append(item, *v.S)
+				item[index] = *v.S
 			}
 		}
 		data = append(data, item)
-	}
-
-	// Convert keys map into a slice, so that, it can be used as a row in data
-	var keys = []string{}
-	for k, _ := range keysMap {
-		keys = append(keys, k)
 	}
 
 	// Scan requests are capped at 1MB of output data, so, we look for LastEvaluatedKey
@@ -136,4 +141,13 @@ func scan(ctx context.Context, db *dynamodb.Client, lek map[string]dynamodb.Attr
 
 func main() {
 	lambda.Start(Handler)
+}
+
+func findIndex(slice []string, item string) int {
+	for i, _ := range slice {
+		if slice[i] == item {
+			return i
+		}
+	}
+	return -1
 }
